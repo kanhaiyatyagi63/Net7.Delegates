159db01c61ed7feb4ae2feaeae9fb55f40b915fb
Kanhaiya Tyagi <kanhaiyatyagi63@gmail.com>
2023-01-01 14:25:04 +0530

    Add project files.

---
+++ Net7.Delegates.Tests/ProgramUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+
+namespace Net7.Delegates.Tests
+{
+    public class ProgramUnitTest
+    {
+        [Fact]
+        public void WhenDisplayMessageDelegateCall_ThenVerifyDisplayMessageDelegateBehaviour()
+        {
+            // arrange
+            var mockGetNames = new Mock<Program.DisplayMessage>();
+            mockGetNames.Setup(_ => _(It.IsAny<string>())).Verifiable();
+
+            // act
+            mockGetNames.Object.Invoke("Hello delegates!");
+
+            // assert
+            mockGetNames.Verify(_ => _.Invoke("Hello delegates!"), Times.Once);
+        }
+
+        [Theory]
+        [InlineData("Kanhaya", "Tyagi", "Kanhaya Tyagi")]
+        [InlineData("Code", "Maze", "Code Maze")]
+        public void WhenFirstAndLastName_ThenGetFullName(string firstName, string lastName, string fullName)
+        {
+            // arrange
+
+            // act
+            var result = Program.GetFullName(firstName, lastName);
+            // assert
+            Assert.Equal(fullName, result);
+        }
+
+        [Theory]
+        [InlineData("Delegates in c#")]
+        [InlineData("Welcome to code maze")]
+
+        public void WhenMessagePassedOnPrintMethod_ThenVerifyingSuccessBehaviour(string message)
+        {
+            try
+            {
+                // arrange
+
+                // act
+                Program.Print(message);
+
+                // assert
+                Assert.True(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+        }
+
+    }
+}
---
+++ Net7.Delegates/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Net7.Delegates
+{
+
+    public class Program
+    {
+        // declaration
+        public delegate void DisplayMessage(string message);
+
+        public static void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public static string GetFullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Delegate
+            // create instance 
+            var displayMessageDelegate = new DisplayMessage(Print);
+
+            // calling delegate
+            displayMessageDelegate.Invoke("Hello delegates!");
+
+
+            // Func
+            // create instance 
+            Func<string, string, string> getFullNameDelegate = GetFullName;
+
+            // invocation
+            var result = getFullNameDelegate.Invoke("Kanhaya", "Tyagi");
+
+            Console.WriteLine(result);
+
+            // Action
+            // create instance 
+            Action<string> actionDelegate = Print;
+
+            // calling delegate
+            actionDelegate.Invoke("Hello delegates!");
+
+        }
+    }
+}
